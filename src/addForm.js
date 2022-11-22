24a30a331bc6a28c65dbb9e36bbce519528fa8de
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-11-22 06:29:23 +0000

    Add popup form w/o function

---
+++ src/addForm.js
@@ -0,0 +1,83 @@
+const addForm = (() => {
+    const createElement = element => {
+        const _element = document.createElement(element);
+        return _element;
+    }
+
+    const setAttribute = (element,type,attribute) => {
+        const _element = createElement(element);
+        _element.setAttribute(type,attribute);
+        return _element;
+    }
+
+    const appendDiv = () => {
+        const _element = setAttribute('div','class','form');
+        popup.appendChild(_element)
+        return _element
+    } 
+
+    const formContainer = appendDiv();
+
+    const appendHeader = name => {
+        const _element = setAttribute('h1','class','header');
+        _element.textContent = name;
+        formContainer.appendChild(_element);
+    }
+
+    const appendLabel = name => {
+        const _element = setAttribute('label','for','title');
+        _element.textContent = name;
+        formContainer.appendChild(_element);
+    }
+
+    const appendInput = () => {
+        const _element = setAttribute('input','id','title');
+        formContainer.appendChild(_element);
+    }
+
+    const appendButtonContainer = () => {
+        const _element = setAttribute('div','class','formButtons')
+        formContainer.appendChild(_element);
+        return _element;
+    }
+
+    const buttonContainer = appendButtonContainer();
+
+    const appendCancelButton = name => {
+        const _element = setAttribute('button','id','cancel');
+        _element.textContent = name;
+        buttonContainer.appendChild(_element);
+    }
+
+    const appendCreateButton = name => {
+        const _element = setAttribute('button','id','create');
+        _element.textContent = name;
+        buttonContainer.appendChild(_element);
+    }
+
+    const fadeContainer = () => {
+        const _element = document.querySelector('#container');
+        _element.classList.add('fade');
+
+        const buttons = document.querySelectorAll('.button')
+        buttons.forEach(button => {
+            button.setAttribute('disabled','true');
+        });
+    }
+    
+    const showForm = () => {
+        appendInput();
+        appendLabel('Title');
+        appendHeader('Add Project');
+        appendButtonContainer();
+        appendCancelButton('Cancel');
+        appendCreateButton('Create');
+        fadeContainer();
+    }
+
+    return {
+        showForm
+    };
+})();
+
+export default addForm
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './styles.css';
+import addForm from './addForm';
 
-console.log('This is working');
+const project = (title,description,dueDate,priority) => {
+
+    return { title, description, dueDate, priority }
+}
+
+const createProject = (() => {
+    const create = (title,description,dueDate,priority) => {
+
+        const _project = project(title,description,dueDate,priority);
+        console.log(_project); 
+    }
+
+    return {
+        create,
+    }
+})();
+
+const eventAdder = (() => {
+    const button = document.querySelector('.add')
+    
+    const addEvent = () => {
+        button.addEventListener('click', );
+    }
+    return {
+        addEvent
+    }
+})();
+
+createProject.create('default','this is test','today','high');
+addForm.showForm();
