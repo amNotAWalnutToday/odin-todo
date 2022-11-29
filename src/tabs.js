dda36ac0683a66607e6e8b5a92aa1122b7932594
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-11-29 01:07:16 +0000

    Add function to switch to new tab that only shows projects due today

--- src/tabs.js
+++ src/tabs.js
@@ -1,9 +1,11 @@
+import { isToday } from "date-fns";
 import createProject from "./project";
 
 const changeTab = (() => {
     
     let tab = '';
 
+    //navbar buttons
     const getButtons = button => {
         return document.querySelector(button)
     }
@@ -18,47 +20,19 @@ const changeTab = (() => {
         
 
         _all.addEventListener('click', changeAll);
-    }
-
-    const mapProject = () => {
-        return createProject.projects.map(project => {
-            return `<div class="card ${project.priority}">
-                        <h2>${project.title}</h2>
-                        <div class="info">
-                            <p><span class="bold">Due:</span> ${project.dueDate}</p>
-                            <div>
-                                <label for="${project.title}" class="bold">Complete?</label>
-                                <input id="${project.title}" type="checkbox">
-                            </div>
-                        </div>
-                    </div>`;
-        });
-    }
-
-    const changeAll = (e) => {
-        tab = 'all';
-        createProject.sortDate();
-        console.log(tab);
-
-        const _page = document.querySelector('#todo');
-        _page.innerHTML = 
-        `<div>
-            <h1>All</h1>
-        </div>`;
-        _page.innerHTML += mapProject();
-
-        const checkbox = document.querySelectorAll('input')
-        checkbox.forEach(box => box.addEventListener('click',  setComplete));
-        console.log(checkbox)
-        setChecked();
+        _today.addEventListener('click', changeToday);
     }
 
     //change completion
+    const getChecked = () => {
+        const checkbox = document.querySelectorAll('[type="checkbox"]')
+        checkbox.forEach(box => box.addEventListener('click',  setComplete));
+    }
+
     const setComplete = (e) => {
         const check = document.querySelectorAll(`[type='checkbox']`);
         
         check.forEach(box => {
-            
             console.log(box);
             const index = createProject.returnIndex(box.id);
             
@@ -81,6 +55,67 @@ const changeTab = (() => {
                 ? box.checked = true
                 : box.checked = false;
         });
+    }
+    //filters
+    const checkToday = (date) => {
+        const _date = date.split('/').reverse();
+        const _newDate = new Date(_date[0],_date[1]-(1),_date[2]);
+
+        return isToday(_newDate);
+    }
+    //maps
+    const mapProject = (filter) => {
+        const _projects = createProject.projects.filter(project => {
+            
+            if (checkToday(project.dueDate) && filter === 'today') return project;
+            if (filter === 'none') return project;
+        })
+
+
+        return _projects.map(project => {
+            return `<div class="card ${project.priority}">
+                        <h2>${project.title}</h2>
+                        <div class="info">
+                            <p><span class="bold">Due:</span> ${project.dueDate}</p>
+                            <div>
+                                <label for="${project.title}" class="bold">Complete?</label>
+                                <input id="${project.title}" type="checkbox">
+                            </div>
+                        </div>
+                    </div>`;
+        });
+    }
+    
+    //tab contents
+    const changeAll = (e) => {
+        tab = 'all';
+        createProject.sortDate();
+        console.log(tab);
+
+        const _page = document.querySelector('#todo');
+        _page.innerHTML = 
+        `<div>
+            <h1>All</h1>
+        </div>`;
+        _page.innerHTML += mapProject('none');
+
+        getChecked();
+        setChecked();
+    }
+
+    const changeToday = (e) => {
+        tab = 'today';
+        console.log(tab);
+
+        const _page = document.querySelector('#todo');
+        _page.innerHTML =
+        `<div>
+            <h1>Today</h1>
+        </div>`; 
+        _page.innerHTML += mapProject('today');
+
+        getChecked();
+        setChecked();
     }
 
     return {
